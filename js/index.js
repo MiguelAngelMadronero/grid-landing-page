b8f1e6bc026eb8f6e6ce139b56e06f3289ae2c7f
Miguel Angel M <mams2208@outlook.com>
2024-04-17 11:16:23 -0500

    mi primer commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+
+    
+
+
+// Obtén el contenedor del menú de navegación
+// const menu = document.querySelector(".main__links");
+
+// Agrega un event listener al contenedor del menú
+// menu.addEventListener("click", function(event) {
+//   Verifica si el clic se hizo en un enlace ('a' con la clase 'nav-link')
+//   if (event.target.matches('a.main__link')) {
+//     Remueve la clase 'activo' de todos los enlaces
+//     const enlaces = document.querySelectorAll(".main__link");
+//     enlaces.forEach(function(link) {
+//       link.classList.remove("main__link--active");
+//     });
+
+//     Agrega la clase 'activo' al enlace clicado
+//     event.target.classList.add("main__link--active");
+//   }
+// });
